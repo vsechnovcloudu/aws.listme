a30a55b80c2e5efdf838623639dcf79f82c1eedf
David Kubec <davesade.42@gmail.com>
2018-08-16 16:26:00 +0200

    Listing instances - initial version.

--- src/listme/index.js
+++ src/listme/index.js
@@ -1,8 +1,45 @@
-var AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
-var SLACK_TOKEN = [process.env.SLACK_TOKEN];
+exports.handler = async function(event, context, callback) {
+  const EC2 = new AWS.EC2();
+  
+  try {
 
-exports.handler = async (event) => {
-    // TODO implement
-    return 'Hello from Lambda!';
+    // TODO: filter based on provided parameters, tags
+    // let params = {
+    //   Filters: [
+    //     {
+    //       Name: "", 
+    //       Values: []
+    //     }
+    //   ]
+    // };
+    // const instancesData = await EC2.describeInstances(params).promise();
+    
+    const instancesData = await EC2.describeInstances().promise();
+    
+    let instancesList ='';
+    
+    instancesData.Reservations.forEach(reservation => {
+      reservation.Instances.forEach(instance => {
+        console.log(instance);
+        instancesList+= 'Name: ' + instance.Tags[0].Value +
+        '  id: ' + instance.InstanceId +
+        '  type: ' + instance.InstanceType +
+        '  Status: ' + instance.State.Name +'\n';
+      });
+    });
+    
+    //console.log(instancesList);
+    
+    let response = {
+      'response_type': 'in_channel',
+      'text': instancesList
+    };
+    
+    callback(null, response);
+    
+  } catch (err) {
+    callback(err.message);
+  }
 };
