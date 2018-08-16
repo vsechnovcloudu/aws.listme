73adf4e858694f301f777bf17513ed887c2283f3
David Kubec <davesade.42@gmail.com>
2018-08-16 13:25:55 +0200

    Method changed to POST - etween API and Lambda it is actually POST.

--- terraform/api.tf
+++ terraform/api.tf
@@ -38,7 +38,7 @@ resource "aws_api_gateway_integration" "listmeget" {
   rest_api_id             = "${aws_api_gateway_rest_api.listme.id}"
   resource_id             = "${aws_api_gateway_resource.listme.id}"
   http_method             = "${aws_api_gateway_method.listmeget.http_method}"
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.REGION}:lambda:path/2015-03-31/functions/${aws_lambda_function.listme.arn}/invocations"
 }
