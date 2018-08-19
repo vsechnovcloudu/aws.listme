2e88362d98cab05232fb06fc9a8d08f714a74c31
David Kubec <davesade.42@gmail.com>
2018-08-19 15:48:50 +0200

    Body mapping template added to API (due GET).

--- terraform/api.tf
+++ terraform/api.tf
@@ -41,6 +41,57 @@ resource "aws_api_gateway_integration" "listmeget" {
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.REGION}:lambda:path/2015-03-31/functions/${aws_lambda_function.listme.arn}/invocations"
+  
+  request_templates {
+      "application/xml" = <<EOF
+      ##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+    ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
+    #set($allParams = $input.params())
+    {
+    "body-json" : $input.json('$'),
+    "params" : {
+    #foreach($type in $allParams.keySet())
+        #set($params = $allParams.get($type))
+    "$type" : {
+        #foreach($paramName in $params.keySet())
+        "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+            #if($foreach.hasNext),#end
+        #end
+    }
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "stage-variables" : {
+    #foreach($key in $stageVariables.keySet())
+    "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "context" : {
+        "account-id" : "$context.identity.accountId",
+        "api-id" : "$context.apiId",
+        "api-key" : "$context.identity.apiKey",
+        "authorizer-principal-id" : "$context.authorizer.principalId",
+        "caller" : "$context.identity.caller",
+        "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+        "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+        "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+        "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+        "http-method" : "$context.httpMethod",
+        "stage" : "$context.stage",
+        "source-ip" : "$context.identity.sourceIp",
+        "user" : "$context.identity.user",
+        "user-agent" : "$context.identity.userAgent",
+        "user-arn" : "$context.identity.userArn",
+        "request-id" : "$context.requestId",
+        "resource-id" : "$context.resourceId",
+        "resource-path" : "$context.resourcePath"
+        }
+    }
+
+  EOF
+    }
+
 }
 
 resource "aws_api_gateway_integration_response" "listmeget" {
