fe66eec076076e4be48b056931fc8844a546e7d5
David Kubec <davesade.42@gmail.com>
2018-08-20 12:51:05 +0200

    Fixed issue with accidental removal of API stages.

--- terraform/api.tf
+++ terraform/api.tf
@@ -43,21 +43,59 @@ resource "aws_api_gateway_integration" "listmeget" {
   uri                     = "arn:aws:apigateway:${var.REGION}:lambda:path/2015-03-31/functions/${aws_lambda_function.listme.arn}/invocations"
   
   request_templates {
-    "application/json" = <<EOF
-    {
-      "text" : "$input.params('text')",
-      "channel" : "$input.params('channel')",
-      "token" : "$input.params('token')",
-      "team_id" : "$input.params('team_id')",
-      "team_domain" : "$input.params('team_domain')",
-      "channel_id" : "$input.params('channel_id')",
-      "channel_name" : "$input.params('channel_name')",
-      "user_id" : "$input.params('user_id')",
-      "user_name" : "$input.params('user_name')",
-      "command" : "$input.params('command')",
-      "response_url" : "$input.params('response_url')"
+      "application/json" = <<EOF
+      {
+        "text" : "$input.params('text')",
+        "channel" : "$input.params('channel')",
+        "token" : "$input.params('token')",
+        "team_id" : "$input.params('team_id')",
+        "team_domain" : "$input.params('team_domain')",
+        "channel_id" : "$input.params('channel_id')",
+        "channel_name" : "$input.params('channel_name')",
+        "user_id" : "$input.params('user_id')",
+        "user_name" : "$input.params('user_name')",
+        "command" : "$input.params('command')",
+        "response_url" : "$input.params('response_url')"
+      }
+  EOF
     }
-    EOF
+
+}
+
+resource "aws_api_gateway_integration_response" "listmeget" {
+  depends_on  = ["aws_api_gateway_method_response.listmeget"]
+  rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
+  resource_id = "${aws_api_gateway_resource.listme.id}"
+  http_method = "${aws_api_gateway_method.listmeget.http_method}"
+  status_code = "${aws_api_gateway_method_response.listmeget.status_code}"
+
+  response_templates {
+    "application/json" = ""
+  }
+
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Origin" = "'${var.ORIGIN}'"
+  }
+}
+
+resource "aws_api_gateway_method" "cors-listme" {
+  rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
+  resource_id = "${aws_api_gateway_resource.listme.id}"
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors-listme" {
+  rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
+  resource_id = "${aws_api_gateway_resource.listme.id}"
+  http_method = "${aws_api_gateway_method.cors-listme.http_method}"
+  type = "MOCK"
+  request_templates {
+      "application/json" = <<EOF
+        {
+        "statusCode" : 200
+        }
+        EOF
   }
 }
 
@@ -91,12 +129,12 @@ resource "aws_api_gateway_method_response" "cors-listme" {
 }
 
 # resource "aws_api_gateway_deployment" "listme" {
-  #   depends_on = ["aws_api_gateway_integration.listmeget"]
-  #   rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
-  #   stage_name = "${var.APISTAGE}"
-  #   stage_description = "Latest"
-  #   stage_description = "Deployed at ${timestamp()}"
-  #   lifecycle {
-    #     create_before_destroy = true
-    #   }
-    # }
+#   depends_on = ["aws_api_gateway_integration.listmeget"]
+#   rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
+#   stage_name = "${var.APISTAGE}"
+#   stage_description = "Latest"
+#   stage_description = "Deployed at ${timestamp()}"
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
