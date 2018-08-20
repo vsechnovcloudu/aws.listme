9c8f3764212019a70c17e18c6bf909749e0fc009
David Kubec <davesade.42@gmail.com>
2018-08-20 10:50:30 +0200

    Simplified body mapping template.

--- terraform/api.tf
+++ terraform/api.tf
@@ -43,91 +43,21 @@ resource "aws_api_gateway_integration" "listmeget" {
   uri                     = "arn:aws:apigateway:${var.REGION}:lambda:path/2015-03-31/functions/${aws_lambda_function.listme.arn}/invocations"
   
   request_templates {
-      "application/json" = <<EOF
-      ##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
-    ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
-    #set($allParams = $input.params())
+    "application/json" = <<EOF
     {
-    "body-json" : $input.json('$'),
-    "params" : {
-    #foreach($type in $allParams.keySet())
-        #set($params = $allParams.get($type))
-    "$type" : {
-        #foreach($paramName in $params.keySet())
-        "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
-            #if($foreach.hasNext),#end
-        #end
+      "text" : "$input.params('text')",
+      "channel" : "$input.params('channel')",
+      "token" : "$input.params('token')",
+      "team_id" : "$input.params('team_id')",
+      "team_domain" : "$input.params('team_domain')",
+      "channel_id" : "$input.params('channel_id')",
+      "channel_name" : "$input.params('channel_name')",
+      "user_id" : "$input.params('user_id')",
+      "user_name" : "$input.params('user_name')",
+      "command" : "$input.params('command')",
+      "response_url" : "$input.params('response_url')"
     }
-        #if($foreach.hasNext),#end
-    #end
-    },
-    "stage-variables" : {
-    #foreach($key in $stageVariables.keySet())
-    "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
-        #if($foreach.hasNext),#end
-    #end
-    },
-    "context" : {
-        "account-id" : "$context.identity.accountId",
-        "api-id" : "$context.apiId",
-        "api-key" : "$context.identity.apiKey",
-        "authorizer-principal-id" : "$context.authorizer.principalId",
-        "caller" : "$context.identity.caller",
-        "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
-        "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
-        "cognito-identity-id" : "$context.identity.cognitoIdentityId",
-        "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
-        "http-method" : "$context.httpMethod",
-        "stage" : "$context.stage",
-        "source-ip" : "$context.identity.sourceIp",
-        "user" : "$context.identity.user",
-        "user-agent" : "$context.identity.userAgent",
-        "user-arn" : "$context.identity.userArn",
-        "request-id" : "$context.requestId",
-        "resource-id" : "$context.resourceId",
-        "resource-path" : "$context.resourcePath"
-        }
-    }
-
-  EOF
-    }
-
-}
-
-resource "aws_api_gateway_integration_response" "listmeget" {
-  depends_on  = ["aws_api_gateway_method_response.listmeget"]
-  rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
-  resource_id = "${aws_api_gateway_resource.listme.id}"
-  http_method = "${aws_api_gateway_method.listmeget.http_method}"
-  status_code = "${aws_api_gateway_method_response.listmeget.status_code}"
-
-  response_templates {
-    "application/json" = ""
-  }
-
-  response_parameters {
-    "method.response.header.Access-Control-Allow-Origin" = "'${var.ORIGIN}'"
-  }
-}
-
-resource "aws_api_gateway_method" "cors-listme" {
-  rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
-  resource_id = "${aws_api_gateway_resource.listme.id}"
-  http_method = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "cors-listme" {
-  rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
-  resource_id = "${aws_api_gateway_resource.listme.id}"
-  http_method = "${aws_api_gateway_method.cors-listme.http_method}"
-  type = "MOCK"
-  request_templates {
-      "application/json" = <<EOF
-        {
-        "statusCode" : 200
-        }
-        EOF
+    EOF
   }
 }
 
@@ -161,12 +91,12 @@ resource "aws_api_gateway_method_response" "cors-listme" {
 }
 
 # resource "aws_api_gateway_deployment" "listme" {
-#   depends_on = ["aws_api_gateway_integration.listmeget"]
-#   rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
-#   stage_name = "${var.APISTAGE}"
-#   stage_description = "Latest"
-#   stage_description = "Deployed at ${timestamp()}"
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  #   depends_on = ["aws_api_gateway_integration.listmeget"]
+  #   rest_api_id = "${aws_api_gateway_rest_api.listme.id}"
+  #   stage_name = "${var.APISTAGE}"
+  #   stage_description = "Latest"
+  #   stage_description = "Deployed at ${timestamp()}"
+  #   lifecycle {
+    #     create_before_destroy = true
+    #   }
+    # }
