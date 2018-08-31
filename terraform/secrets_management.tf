cd1c447a55d85033f5fb0f9970bd07a177cbf0f2
David Kubec <davesade.42@gmail.com>
2018-08-31 23:50:47 +0200

    Added policies for KMS and secrets.

--- terraform/secrets_management.tf
+++ terraform/secrets_management.tf
@@ -1,6 +1,7 @@
 resource "aws_kms_key" "secretmanagement" {
   description             = "This key is used to encrypt secrets"
   deletion_window_in_days = 10
+  policy                  = "${aws_iam_policy.accesskms.arn}"
 }
 
 resource "aws_kms_alias" "secretmanagement" {
@@ -14,4 +15,46 @@ data "aws_secretsmanager_secret" "slacksecret" {
 
 data "aws_secretsmanager_secret_version" "slacksecret" {
   secret_id     = "slack/signisecret"
+}
+
+data "aws_iam_policy_document" "accesskms" {
+
+  statement {
+    sid = "1"
+
+    actions = [
+      "kms:DescribeKey"
+    ]
+    
+    resources = ["${data.aws_kms_key.secretmanagement.arn}"]
+  }
+}
+
+resource "aws_iam_policy" "accesskms" {
+  depends_on = ["aws_kms_key.secretmanagement"]
+  name   = "listme-accesskms-${terraform.workspace}"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.accesskms.json}"
+  description = "Policy allowing to access KMS key for secrets decryption."
+}
+
+data "aws_iam_policy_document" "accesssecrets" {
+
+  statement {
+    sid = "1"
+
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    
+    resources = ["${data.aws_secretsmanager_secret.slacksecret.arn}"]
+  }
+}
+
+resource "aws_iam_policy" "accesssecrets" {
+  depends_on = ["aws_secretsmanager_secret.slacksecret"]
+  name   = "listme-accesssecrets-${terraform.workspace}"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.accesssecrets.json}"
+  description = "Policy allowing to access secrets in the vault."
 }
