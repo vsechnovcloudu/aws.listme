9f8cca8c259bec45ff27a4f58749c87c182c828b
David Kubec <davesade.42@gmail.com>
2018-08-31 15:36:25 +0200

    Added secret into TF template.

--- terraform/secrets_management.tf
+++ terraform/secrets_management.tf
@@ -7,3 +7,7 @@ resource "aws_kms_alias" "secretmanagement" {
   name          = "alias/listme/secrets"
   target_key_id = "${aws_kms_key.secretmanagement.key_id}"
 }
+
+data "aws_secretsmanager_secret" "slacksecret" {
+  name = "slack/signisecret"
+}
