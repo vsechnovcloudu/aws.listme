d91699d978fc500097d6cd84b3da2c2b4a63afbf
David Kubec <davesade.42@gmail.com>
2018-08-31 15:30:14 +0200

    Added KMS key for secrets.

---
+++ terraform/secrets_management.tf
@@ -0,0 +1,9 @@
+resource "aws_kms_key" "secretmanagement" {
+  description             = "This key is used to encrypt secrets"
+  deletion_window_in_days = 10
+}
+
+resource "aws_kms_alias" "secretmanagement" {
+  name          = "alias/listme/secrets"
+  target_key_id = "${aws_kms_key.secretmanagement.key_id}"
+}
